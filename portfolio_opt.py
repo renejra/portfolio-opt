6937b8ed643e92e951f8a7803fe705f856ab4495
renejra <rjriveroarrieta@gmail.com>
2023-01-26 21:26:22 +0100

    progress bar and frontend

--- portfolio_opt.py
+++ portfolio_opt.py
@@ -7,10 +7,10 @@ import plotly.express as px
 
 
 st.header('portfolio-opt')
-st.subheader('Portfolio Optimization app')
+st.subheader('An app for portfolio optimization')
 st.write(
     """
-    A Python application to help portfolio managers optimize their asset mix using streamlit and yfinance.
+    A Python application to help portfolio managers optimize their asset mix built on top of streamlit and yfinance.
 
     Modern Portfolio Theory suggests that investors should diversify by 
     investing in a range of different assets in order to reduce the overall portfolio risk.
@@ -22,27 +22,28 @@ st.write(
 
     This app aims to help investors **ideate a balanced portfolio mix**
     allowing you to **enter a range of instruments and show you interactive insights**. 
-    You could use this f.e. to help prevent being overly weighted in a single asset class or sector, 
-    while maintaining or improving your `expected returns`.
+    You could use this to help *prevent being overly weighted in a single asset class or sector, 
+    while maintaining or improving your expected returns*.
 
-    This app could ultimately help portfolio managers **plan to good expected return, respective to their risk apetite**.
+    This app could ultimately help portfolio managers and investors **diversify to improve expected returns, in respect to their risk apetite**.
     """
 )
 st.subheader('Get Started')
 st.write('Pass a list of symbols and a number of experiments to start. You need to look for the tickers in https://finance.yahoo.com/')
-products = st.text_input("Enter symbols to look for (f.e.): okta msft ^gspc imkta ^ixic gold si=f dax aapl goog", value="amzn aapl goog")
+products = st.text_input("Enter symbols to look for (f.e.): `okta msft ^gspc imkta ^ixic gold si=f dax aapl goog`", value="amzn aapl goog")
 
 col1, col2 = st.columns(2)
 with col1:
     start_date = col1.text_input("Enter a start date (YYYY-MM-DD)", value="2021-01-01")
-    num_ports = col1.number_input('No. of simulations: ', 1000, 100000, value=10000)
+    num_sims = col1.number_input('No. of simulations: ', 1000, 100000, value=10000)
 
 with col2:
     end_date = col2.text_input("Enter an end date: (YYYY-MM-DD)", value="2023-01-31")
     sd = st.number_input('Seed: ', 1, 999, value=101)
     np.random.seed(sd)
 
-if col2.button('Run portfolio optimization!'):
+# @st.experimental_memo(suppress_st_warning=True)
+def run_simulation(num_sims: int) -> None:
     st.write('---')
     data = yf.download(products, start=start_date, end=end_date)
     stocks = data.Close
@@ -57,30 +58,42 @@ if col2.button('Run portfolio optimization!'):
 
     st.subheader('Daily Returns Statitics')
     st.write(stock_daily_ret.describe().T)
-    st.bar_graph(stock_daily_ret)
+
+    for symbol in stock_daily_ret.columns:
+        hist = px.histogram(stock_daily_ret, x=symbol)
+        st.plotly_chart(hist)
 
     weights = np.array(np.random.random(stocks.shape[1]))
     weights = weights / np.sum(weights)
     exp_ret = np.sum(log_ret.mean() * weights) *252
     exp_vol = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov() * 252, weights)))
-    all_weights = np.zeros((num_ports,len(stocks.columns)))
-    ret_arr = np.zeros(num_ports)
-    vol_arr = np.zeros(num_ports)
-    sharpe_arr = np.zeros(num_ports)
+    all_weights = np.zeros((num_sims,len(stocks.columns)))
+    ret_arr = np.zeros(num_sims)
+    vol_arr = np.zeros(num_sims)
+    sharpe_arr = np.zeros(num_sims)
 
-    for ind in range(num_ports):
-        weights = np.array(np.random.random(stocks.shape[1])) # Create Random Weights
-        weights = weights / np.sum(weights)
-        all_weights[ind,:] = weights
+    # with st.spinner('Running simulations!'):
+    #     pass
 
-        ret_arr[ind] = np.sum((log_ret.mean() * weights) *252)
-        vol_arr[ind] = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov() * 252, weights)))
-        sharpe_arr[ind] = ret_arr[ind]/vol_arr[ind]
+    st.write("---")
+    with st.empty():
+        for ind in range(num_sims):
+            # st.progress(ind/num_sims)
+            weights = np.array(np.random.random(stocks.shape[1])) # Create Random Weights
+            weights = weights / np.sum(weights)
+            all_weights[ind,:] = weights
+
+            ret_arr[ind] = np.sum((log_ret.mean() * weights) *252)
+            vol_arr[ind] = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov() * 252, weights)))
+            sharpe_arr[ind] = ret_arr[ind]/vol_arr[ind]
+            st.write(f'**Running simulation** ... {round(ind*100/num_sims)} % complete')
+        st.balloons()
 
     weights_df = pd.DataFrame(all_weights, columns = stocks.columns)
     results = pd.concat([(weights_df*100).round(2), pd.Series(vol_arr, name='volatility'), pd.Series(ret_arr, name='returns')], axis=1)
     results['sharpe_ratio'] = results.returns/results.volatility
 
+    st.write('---')
     st.subheader('Pick the best mix for your risk apetite')
     st.write("""
             This is the core result of this application. The idea is that you find a mix of the stocks you picked,
@@ -99,3 +112,7 @@ if col2.button('Run portfolio optimization!'):
         and therefore, the author can't be made responsible for their outcomes.
         """
     )
+
+
+if col2.button('Run portfolio optimization!'):
+    run_simulation(num_sims)
